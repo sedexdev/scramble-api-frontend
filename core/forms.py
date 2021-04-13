b50918ababddd43aca4c82d67419395a8bbf7cba
Andrew Macmillan <andrew.macmillan@ymail.com>
2021-04-13 15:33:09 +0100

    create basic API form

---
+++ core/forms.py
@@ -0,0 +1,48 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import RadioField, SelectField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class APIForm(FlaskForm):
+
+    service = SelectField(
+        'Service Type',
+        choices=[
+            ('encrypt', 'Encryption'),
+            ('hash', 'Hashing')
+        ],
+        validators=[DataRequired()])
+
+    encryption = RadioField(
+        'Encryption Types',
+        choices=[
+            ('rsa', 'RSA'),
+            ('aes', 'AES')
+        ])
+
+    hashing = RadioField(
+        'Hashing Algorithm',
+        choices=[
+            ('sha256', 'SHA3-256'),
+            ('sha512', 'SHA3-512'),
+        ]
+    )
+
+    plaintext = SelectField(
+        'Plaintext',
+        choices=[
+            ('text', 'Text'),
+            ('file', 'File')
+        ],
+        validators=[DataRequired()])
+
+    text = TextAreaField('Plaintext')
+
+    file_upload = FileField(
+        'Upload file',
+        validators=[
+            FileRequired(),
+            FileAllowed(['.txt', '.pdf', '.docx'], 'Text files only!')])
+
+    submit = SubmitField('Submit')
--- core/views.py
+++ core/views.py
@@ -1,15 +1,30 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template
+from .forms import APIForm
+import requests
 
 core_blueprint = Blueprint(
     'core',
     __name__,
     template_folder='templates')
 
+url = 'www.example.com'
+
 
 @core_blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        pass
-    return render_template(
-        'index.html',
-        title='Index')
+    form = APIForm()
+    if form.validate_on_submit():
+        res = requests.post(url, data={
+            'service': form.serivce.data,
+            'encryption': form.encryption.data,
+            'hashing': form.hashing.data,
+            'plaintext': form.plaintext.data,
+            'text': form.text.data,
+            'file': form.file_upload.data
+        })
+        if res.status_code == 200:
+            flash(res.message, 'message')
+        else:
+            flash(res.message, 'warning')
+        return render_template('index.html', title='Scramble API', form=form)
+    return render_template('index.html', title='Scramble API', form=form)

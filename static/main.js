8dfb2da29fb67600cbe025c5509145e483391072
Andrew Macmillan <andrew.macmillan@ymail.com>
2021-04-18 18:47:58 +0100

    made app responsive

--- static/main.js
+++ static/main.js
@@ -3,4 +3,28 @@
     if (email) {
         email.focus();
     }
+
+    const setClasses = (el, toGo, toAdd) => {
+        el.classList.remove(toGo);
+        el.classList.add(toAdd);
+    }
+
+    window.addEventListener('resize', () => {
+        const windowWidth = window.innerWidth;
+        const mainHeader = document.getElementById('main-header');
+        const coreBannerTitle = document.getElementById('app-name');
+        const coreHeading = document.getElementById('core-heading');
+        
+        if (mainHeader && coreBannerTitle && coreHeading) {
+            if (windowWidth <= 700) {
+                setClasses(mainHeader, 'main-header', 'main-header-resize');
+                setClasses(coreBannerTitle, 'app-name-container', 'invisible');
+                setClasses(coreHeading, 'invisible', 'form-heading');
+            } else {
+                setClasses(mainHeader, 'main-header-resize', 'main-header');
+                setClasses(coreBannerTitle, 'invisible', 'app-name-container');
+                setClasses(coreHeading, 'form-heading', 'invisible');
+            }  
+        }
+    })
 })();

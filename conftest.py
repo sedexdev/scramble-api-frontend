eb6e71b07529b72e66e1b2f7677b3cfc712c3e21
Andrew Macmillan <andrew.macmillan@ymail.com>
2021-05-17 10:43:55 +0100

    remove db ref

--- conftest.py
+++ conftest.py
@@ -6,6 +6,6 @@ from run import create_app
 @pytest.fixture
 def client():
     config = os.environ['APP_SETTINGS']
-    app, db = create_app(config)
+    app = create_app(config)
     with app.test_client() as client:
         yield client

37d7a26abe2bb0d20093b0422b1f7635e1b55f0d
Andrew Macmillan <andrew.macmillan@ymail.com>
2021-05-18 09:22:24 +0100

    test all endpoints return 200 #5

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,3 +24,33 @@ def test_reset_returns_200(client: FlaskClient) -> None:
 def test_update_returns_200(client: FlaskClient) -> None:
     res = client.get('/update')
     assert res.status_code == 200
+
+
+def test_account_returns_200(client: FlaskClient) -> None:
+    res = client.get('/account')
+    assert res.status_code == 200
+
+
+def test_admin_login_returns_200(client: FlaskClient) -> None:
+    res = client.get('/admin/login')
+    assert res.status_code == 200
+
+
+def test_admin_update_returns_200(client: FlaskClient) -> None:
+    res = client.get('/admin/update')
+    assert res.status_code == 200
+
+
+def test_hashing_returns_200(client: FlaskClient) -> None:
+    res = client.get('/hashing')
+    assert res.status_code == 200
+
+
+def test_encryption_returns_200(client: FlaskClient) -> None:
+    res = client.get('/encryption')
+    assert res.status_code == 200
+
+
+def test_results_returns_200(client: FlaskClient) -> None:
+    res = client.get('/results')
+    assert res.status_code == 200
